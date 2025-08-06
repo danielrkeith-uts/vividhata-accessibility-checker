a8e7a56f051146f043ed6d180346b99f8044a4e9
Daniel Keith <danielrkeith@mac.lan>
2025-08-06 19:05:28 +1000

    API with HelloWorldController

---
+++ accessibility-api/src/test/java/co/vividhata/accessibility_api/AccessibilityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.vividhata.accessibility_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessibilityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
