be62d72032754d705788001ed28df3c48782fea0
Daniel Keith <danielrkeith@Neptune.local>
2025-09-03 16:00:13 +1000

    Fixed

--- accessibility-api/src/main/resources/postgresql/rebuild_schema.sql
+++ accessibility-api/src/main/resources/postgresql/rebuild_schema.sql
@@ -4,9 +4,8 @@ CREATE SCHEMA ac;
 
 CREATE TABLE ac.account (
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    username VARCHAR(100),
+    username VARCHAR(100) UNIQUE,
     password VARCHAR(100),
     first_name VARCHAR(100),
-    last_name VARCHAR(100),
-    UNIQUE (username, password)
+    last_name VARCHAR(100)
 );
