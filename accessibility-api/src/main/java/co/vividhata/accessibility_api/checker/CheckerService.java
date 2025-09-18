3bcc7b7f442690d2b8b74130cfa956169b2a2f90
danielrkeith-uts <daniel.r.keith@student.uts.edu.au>
2025-09-18 12:41:03 +1000

    Dependency injection for IIssueChecker implementation (#17)
    Co-authored-by: Daniel Keith <danielrkeith@Neptune.local>

--- accessibility-api/src/main/java/co/vividhata/accessibility_api/checker/CheckerService.java
+++ accessibility-api/src/main/java/co/vividhata/accessibility_api/checker/CheckerService.java
@@ -1,7 +1,7 @@
 package co.vividhata.accessibility_api.checker;
 
-import co.vividhata.accessibility_api.checker.issue_checkers.SampleIssueChecker;
 import co.vividhata.accessibility_api.model.Issue;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.w3c.dom.Document;
 
@@ -11,17 +11,15 @@ import java.util.List;
 @Service
 public class CheckerService implements ICheckerService {
 
-    private final List<IIssueChecker> checkers = List.of(
-            new SampleIssueChecker(),
-            new SampleIssueChecker()
-    );
+    @Autowired
+    private List<IIssueChecker> issueCheckers;
 
     @Override
     public List<Issue> checkAll(Document document) {
         List<Issue> issues = new ArrayList<>();
 
-        for (IIssueChecker checker : checkers) {
-            issues.addAll(checker.check(document));
+        for (IIssueChecker issueChecker : issueCheckers) {
+            issues.addAll(issueChecker.check(document));
         }
 
         return issues;
