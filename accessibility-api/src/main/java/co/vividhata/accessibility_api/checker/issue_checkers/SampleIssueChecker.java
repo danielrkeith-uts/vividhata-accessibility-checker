7183873edf8388fad901c1acced2abe74ec636ac
Daniel Keith <danielrkeith@Neptune.local>
2025-09-18 12:40:00 +1000

    Dependency injection for IIssueChecker implementation

--- accessibility-api/src/main/java/co/vividhata/accessibility_api/checker/issue_checkers/SampleIssueChecker.java
+++ accessibility-api/src/main/java/co/vividhata/accessibility_api/checker/issue_checkers/SampleIssueChecker.java
@@ -3,26 +3,17 @@ package co.vividhata.accessibility_api.checker.issue_checkers;
 import co.vividhata.accessibility_api.checker.IIssueChecker;
 import co.vividhata.accessibility_api.model.Issue;
 import co.vividhata.accessibility_api.model.IssueType;
+import org.springframework.stereotype.Service;
 import org.w3c.dom.Document;
 
 import java.util.List;
-import java.util.Random;
 
+@Service
 public class SampleIssueChecker implements IIssueChecker {
-
-    private final Random random = new Random();
 
     @Override
     public List<Issue> check(Document document) {
-
-        IssueType issueType = switch (random.nextInt(3)) {
-            case 0 -> IssueType.SAMPLE_ISSUE_1;
-            case 1 -> IssueType.SAMPLE_ISSUE_2;
-            case 2 -> IssueType.SAMPLE_ISSUE_3;
-            default -> null;
-        };
-
-        return List.of(new Issue(-1, -1, issueType, "<sample>snippet</sample>"));
+        return List.of(new Issue(-1, -1, IssueType.SAMPLE_ISSUE, "<sample>snippet</sample>"));
     }
 
 }
