a647aa18be476816e7f3aaa8b4ca83724c2e52f0
Daniel Keith <danielrkeith@mac.lan>
2025-09-26 17:43:09 +1000

    Implemented ScanController endpoints according to ILinkService

---
+++ accessibility-api/src/main/java/co/vividhata/accessibility_api/model/Link.java
@@ -0,0 +1,3 @@
+package co.vividhata.accessibility_api.model;
+
+public record Link(int scanId, String link) { }
--- accessibility-api/src/main/java/co/vividhata/accessibility_api/scan/dto/ScanResponse.java
+++ accessibility-api/src/main/java/co/vividhata/accessibility_api/scan/dto/ScanResponse.java
@@ -1,8 +1,9 @@
 package co.vividhata.accessibility_api.scan.dto;
 
 import co.vividhata.accessibility_api.model.Issue;
+import co.vividhata.accessibility_api.model.Link;
 import co.vividhata.accessibility_api.model.Scan;
 
 import java.util.List;
 
-public record ScanResponse(Scan scan, List<Issue> issues) { }
+public record ScanResponse(Scan scan, List<Issue> issues, List<Link> links) { }
