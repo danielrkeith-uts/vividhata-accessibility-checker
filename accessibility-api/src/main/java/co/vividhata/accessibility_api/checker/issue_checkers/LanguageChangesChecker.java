8e97ea476d02f651328f2c490651d454b64fd991
Phoebe Lowe <phoebe.lowee@gmail.com>
2025-10-16 12:06:55 +1100

    fixing false positives

--- accessibility-api/src/main/java/co/vividhata/accessibility_api/checker/issue_checkers/LanguageChangesChecker.java
+++ accessibility-api/src/main/java/co/vividhata/accessibility_api/checker/issue_checkers/LanguageChangesChecker.java
@@ -41,9 +41,11 @@ public class LanguageChangesChecker implements IIssueChecker {
 
             if (language.isEmpty()) {
                 String textContent = element.getTextContent().trim();
-                if (textContent.length() > 3 && defaultLanguage != null) {
+                if (textContent.length() > 3 && textContent.matches(".*[a-zA-Z].*") && defaultLanguage != null) {
+                    String htmlLang = defaultLanguage.split("-")[0].toLowerCase();
                     Language detected = detector.detectLanguageOf(textContent);
-                    if (!detected.toString().equalsIgnoreCase(defaultLanguage)) {
+                    String detectedLang = detected.toString().substring(0,2).toLowerCase();
+                    if (!detectedLang.equals(htmlLang)) {
                         issues.add(new Issue(-1, -1, ISSUE_TYPE, nodeParser.nodeToHtml(element)));
                     }
                 }
